383767f543e4ca75999b1ce6de1de4380d1bcaa2
Llerena4 <sergioalberto.informatica@gmail.com>
2024-09-26 18:48:51 +0200

    se anade grafico

--- src/app/modules/proveedores/components/proveedores/proveedores.component.ts
+++ src/app/modules/proveedores/components/proveedores/proveedores.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Chart, LinearScale, registerables } from 'chart.js';
+
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-proveedores',
   templateUrl: './proveedores.component.html',
   styleUrl: './proveedores.component.css'
 })
-export class ProveedoresComponent {
+export class ProveedoresComponent implements OnInit{
 
+  facturas: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.loadData();
+  }
+
+
+  loadData() {
+    this.http.get<any>('assets/data/facturas.json').subscribe(data => {
+      this.facturas = data.facturas;
+      console.log(this.facturas)
+      this.createChart();
+    });
+  }
+
+  createChart() {
+    const ctx = document.getElementById('myChart') as HTMLCanvasElement;
+    const myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: this.facturas.map(f => `${f.proveedorName} (${f.expirationDate})`),
+        datasets: [{
+          label: 'Importe a pagar',
+          data: this.facturas.map(f => f.totalAmount),
+          borderColor: 'rgba(75, 192, 192, 1)',
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
 }
